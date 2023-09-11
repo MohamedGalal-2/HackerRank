2965c8bea056c16f44a45dd69ba160b4ed815a68
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-09-11 20:03:57 +0300

    Create query.sql

---
+++ SQL/Weather Observation Station 9/query.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT (CITY LIKE 'A%' OR CITY LIKE 'E%' OR City LIKE 'I%' 
+OR CITY LIKE 'O%' OR CITY LIKE 'U%');
