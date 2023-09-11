372c99c57a3777e4b551bcba3354c2adcff6e84e
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-09-11 19:54:19 +0300

    Create query.sql

---
+++ SQL/Weather Observation Station 6/query.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT City
+FROM Station
+WHERE City LIKE 'A%' OR City LIKE 'E%' OR City LIKE 'I%' 
+OR City LIKE 'O%' OR City LIKE 'U%';
