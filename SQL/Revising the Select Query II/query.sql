be4f20283b03ffe3cff344d9c8cef23e1939be71
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-09-11 19:27:33 +0300

    Create query.sql

---
+++ SQL/Revising the Select Query II/query.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT NAME FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
