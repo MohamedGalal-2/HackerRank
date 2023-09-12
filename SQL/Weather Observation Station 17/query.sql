114da13a5c987620cf6eafd167b401b0f36b3ee7
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-09-12 10:32:41 +0300

    Create query.sql

---
+++ SQL/Weather Observation Station 17/query.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT FORMAT(ROUND(LONG_W,4),'#.0000')
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);
