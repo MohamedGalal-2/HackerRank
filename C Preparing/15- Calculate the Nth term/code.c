bb72f38c1601f8bb8c0874864835f7005e35654c
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-18 21:57:55 +0300

    Added two prepare problems

--- C Preparing/15- Calculate the Nth term/code.c
+++ C Preparing/15- Calculate the Nth term/code.c
@@ -2,33 +2,32 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-//Complete the following function.
 
+//Complete the following function.
 int find_nth_term(int n, int a, int b, int c) {
 	//Write your code here.
-	if (n == 1) {
+	if (1 == n) {
 		return a;
 	}
-	else if (n == 2) {
+	else if (2 == n) {
 		return b;
 	}
-	else if (n == 3) {
+	else if (3 == n) {
 		return c;
 	}
-
 	else {
-		return find_nth_term(n - 1, a, b, c);
-
+		return find_nth_term(n - 1, a, b, c) + find_nth_term(n - 2, a, b, c) + find_nth_term(n - 3, a, b, c);
 	}
-
 }
 
 int main() {
 	int n, a, b, c;
 
-	scanf_s("%d %d %d %d", &n, &a, &b, &c);
+	scanf("%d %d %d %d", &n, &a, &b, &c);
 	int ans = find_nth_term(n, a, b, c);
 
 	printf("%d", ans);
 	return 0;
 }
+
+
