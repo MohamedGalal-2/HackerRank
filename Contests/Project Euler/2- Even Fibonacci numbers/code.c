685ff3c77bb89861122b52c4f93528abffd6d2c3
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-18 20:14:05 +0300

    adjusted the spacings

--- Contests/Project Euler/2- Even Fibonacci numbers/code.c
+++ Contests/Project Euler/2- Even Fibonacci numbers/code.c
@@ -7,20 +7,13 @@
 #include <stdbool.h>
 #include <math.h>
 
-
-
 int main() {
 	int t;
 	long long sum_even = 0;
 	long double gold_rat = (1 + sqrt(5)) / 2;
 	long long Fibonacci_sequence[82] = { 0, 1, 1, 2 };
 	for (int i = 2; i <= 81; i++) {
-
-		//Fibonacci_sequence[i] = (pow(gold_rat, i) - pow(1 - gold_rat, i)) / sqrt(5);
-
 		Fibonacci_sequence[i] = Fibonacci_sequence[i - 1] + Fibonacci_sequence[i - 2];
-
-		printf("%i-      %lld\n", i, Fibonacci_sequence[i]);
 	}
 
 	scanf_s("%d", &t);
@@ -30,7 +23,7 @@ int main() {
 			scanf_s("%lld", &n);
 			if (n >= 10 && n <= 40000000000000000) {
 				for (int i = 2; i <= 81; i++) {
-					if ((long long)Fibonacci_sequence[i] % 2 == 0 && Fibonacci_sequence[i] <= n) {
+					if (Fibonacci_sequence[i] % 2 == 0 && Fibonacci_sequence[i] <= n) {
 						sum_even += Fibonacci_sequence[i];
 					}
 				}
